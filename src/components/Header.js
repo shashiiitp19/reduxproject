fc3834d401023e9de17551dcd95b144bdbd05b32
Shashi Kumar <76843511+shashiiitp19@users.noreply.github.com>
2022-09-28 14:49:25 +0530

    My First Commit

---
+++ src/components/Header.js
@@ -0,0 +1,34 @@
+import classes from "./Header.module.css";
+import { useSelector, useDispatch } from "react-redux/es/exports";
+import { authActions } from "../store/indexReduxToolkit";
+
+const Header = () => {
+  const isAuth = useSelector(state => state.auth.isAuthenticated);
+  const dispatch = useDispatch();
+  const onClickHandler = () =>{
+    dispatch(authActions.logout());
+  };
+
+  return (
+    <header className={classes.header}>
+      <h1>Redux Auth</h1>
+      {isAuth && (
+        <nav>
+          <ul>
+            <li>
+              <a href="/">My Products</a>
+            </li>
+            <li>
+              <a href="/">My Sales</a>
+            </li>
+            <li>
+              <button onClick={onClickHandler}>Logout</button>
+            </li>
+          </ul>
+        </nav>
+      )}
+    </header>
+  );
+};
+
+export default Header;
